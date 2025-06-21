ce91ba705d8af60f70fd199357e68cd56d70083b
Bohemianmage <jeanclaudes_1992@hotmail.com>
2025-06-21 10:24:30 -0600

    Revisión de vulnerabilidad

--- utils/mailer.js
+++ utils/mailer.js
@@ -42,7 +42,7 @@ async function enviarCorreoReserva(reserva, qrCode) {
     // Nombres según idioma
     const tiposHabitacion = {
       1: { es: 'Suite Junior', en: 'Junior Suite' },
-      2: { es: 'Suite Master', en: 'Master Suite' },
+      2: { es: 'Suite Maestra', en: 'Master Suite' },
       3: { es: 'Suite con Balcón', en: 'Suite with Balcony' },
     };
 
